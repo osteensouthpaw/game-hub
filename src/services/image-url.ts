138230f2f68b8c608adef93dcab687ae5da06c50
osteensouthpaw <osteensouthpaw@gmail.com>
2024-06-17 05:01:18 +0300

    handling games without images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholder from "../assets/place-holder-image.png";
+
 const getCroppedUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
